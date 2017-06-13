3b8916f03e4164775dea5a3a64dfac1a9d571aa4
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-06-13 23:32:16 +0300

    Final commit!
    4 days..

--- src/js/common.js
+++ src/js/common.js
@@ -10,9 +10,27 @@
 // 2. При попытке выйти со страницы должен появляться поп-ап ( 2 бутылки в подарок).
 // 3. Желательно сделать адаптив – мобайл -таблет
 
+// Active price card
 $(".price_card").click(function() {
   $(".price_card").removeClass("active_price_card");
   $(".price_card").find(".price_card_status_p").html("select package");
+  $(".lady_popup").css("opacity","0");
   $(this).addClass("active_price_card");
   $(this).find(".price_card_status_p").html("selected package");
 });
+$("#price_card_2").click(function() {
+  $(".lady_popup").css("opacity","1");
+});
+
+// Lady popup
+$(document).ready(function() {
+  setTimeout(function () {
+    $(".lady_popup").css("opacity","1")
+  }, 2000)
+});
+
+
+// Open modal on tab close
+window.onbeforeunload = function() {
+  return $('#myModal').modal('show');
+};
